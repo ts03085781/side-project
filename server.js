fe4e2c84738139f79e9f23d7aeb6c74e0f8299ee
YiLin <ts03085781@gmail.com>
2020-02-10 17:59:52 +0800

    新增click game 的 add reank API

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ app.use(function (req, res, next) {
 });
 
 //建立DB數據模板
-const testDb = mongoose.model('user1',mongoose.Schema({name:String,point:Number}));
+const user1 = mongoose.model('user1',mongoose.Schema({name:String,point:Number}));
+const game_ranks = mongoose.model('game_ranks',mongoose.Schema({name:String,frequency:Number,comment:String}));
 
 //監聽8000窗口
 app.listen(port,ip, ()=>{console.log(`SERVER created at ${ip}`);})
@@ -49,47 +50,56 @@ app.use(bodyParser.json())
 
 //增加 mongoDB 內的 document
 app.post('/api/post', async (req,res) =>{
-    const checkData = await testDb.find({name:req.body.name})
+    const checkData = await user1.find({name:req.body.name})
     if(checkData.length>0){
         res.send(`重複資料!,資料庫內已存有"${req.body.name}"`)
     }else{
-        const newData = new testDb({
+        const newData = new user1({
             name:req.body.name,
             point:req.body.point,
         });
         await newData.save();
         res.send(`已成功添加${req.body.name},${req.body.point}公斤`)
     }
-
-
 })
 
 // 刪除指定name
 app.delete('/api/delete/:name', async (req, res) =>{
-    await testDb.deleteOne({name:req.params.name})
+    await user1.deleteOne({name:req.params.name})
     res.send(`name:${req.params.name} is deleted`)
 })
 
 //刪除全部
 app.delete('/api/delete/all', async (req, res) =>{
-    await testDb.remove({})
+    await user1.remove({})
     res.send(`all deleted`)
 })
 
 //查詢全部name
 app.get('/api/get/all', async (req,res) =>{
-    const data = await testDb.find()
+    const data = await user1.find()
     res.json(data)
 })
 
 //查詢單一name
 app.get('/api/get/:name', async (req,res) =>{
-    const data = await testDb.find({name:req.params.name})
+    const data = await user1.find({name:req.params.name})
     res.json(data)
 })
 
 //修改原有資料
 app.put('/api/update', async (req, res)=>{
-    const data = await testDb.update({name:req.body.name},{$set:{point:req.body.point}})
+    const data = await user1.update({name:req.body.name},{$set:{point:req.body.point}})
     res.json(data)
+})
+
+//點擊遊戲rank加入名單
+app.post('/api/clickGame/post', async (req,res) =>{
+        const newGameData = new game_ranks({
+            name:req.body.name,
+            frequency:req.body.frequency,
+            comment:req.body.comment,
+        });
+        await newGameData.save();
+        res.send(`已成功加入rank排名公斤`)
 })
